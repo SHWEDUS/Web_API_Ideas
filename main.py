ce3c816c57487a8a6fb16a9a4a94d0a77780d621
Крылов Глеб Иванович <krylov1243@gmail.com>
2023-12-25 08:46:30 +0500

    hotfix jinja

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from crud import get_directions, get_queries
 import models
+from sqlalchemy.orm import Session
+from database import get_db
 
 from database import engine
-from routes import router_websocket, router_directions, router_queries
+from routes import read_directions, router_websocket, router_directions, router_queries
 
 # Создание таблиц в БД
 models.Base.metadata.create_all(bind=engine)
@@ -20,15 +23,20 @@ app = FastAPI(
 
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    http_protocol = request.headers.get("x-forwarded-proto", "http")
+async def read_root(request: Request, db: Session = Depends(get_db)):
+    http_protocol = request.headers.get("x-forwarded-proto", "https")
+    directions = get_directions(db, skip=0, limit=10)
+    queries = get_queries(db, skip=0, limit=10)
     ws_protocol = "wss" if http_protocol == "https" else "ws"
     server_urn = request.url.netloc
     return templates.TemplateResponse("index.html",
                                       {"request": request,
                                        "http_protocol": http_protocol,
                                        "ws_protocol": ws_protocol,
+                                       "directions": directions,
+                                       "queries": queries,
                                        "server_urn": server_urn})
+
 
 
 # Подключаем созданные роутеры в приложение
